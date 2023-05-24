587ff8297bbd5d7ab83667fa8400e25b2483ce4f
Jessica Severino <jessicaa.severinoo@gmail.com>
2023-05-24 19:22:00 -0300

    feat: modulo ciclo do observable

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,15 +7,23 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
   protected campoBusca: string = '';
   protected listaLivros: [];
+
+  private subscription: Subscription;
 
   constructor(private service: LivroService) {}
 
   buscarLivros() {
-    this.service
-      .buscar(this.campoBusca)
-      .subscribe((retornoApi) => console.log(retornoApi));
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: (retornoApi) => console.log(retornoApi),
+      error: (error) => console.log('Error', error),
+      complete: () => console.log('complete'),
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
