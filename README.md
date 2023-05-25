e5e68117b0eab1f3d73399aea9f3839c5a7c228e
Jessica Severino <jessicaa.severinoo@gmail.com>
2023-05-24 21:28:27 -0300

    feat: modulo otimizando a busca com rxjs

--- README.md
+++ README.md
@@ -38,13 +38,31 @@ Conteúdo:
 - Passar parâmetros e utilizar pipes encadeados;
 - Criar um pipe customizado.
 
+## Módulo 05: Otimizando a busca com RxJS
+
+Conteúdo
+
+- Desenvolver uma busca dinâmica;
+- Utilizar o pipe async;
+- Encadear os operadores switchMap, filter e debounceTime para otimizar a performance da aplicação.
+
+## Anotações extras
+
 #### Colinha dos operadores
 
-Pipe- Função que serve para agrupar múltiplos operadores. Não modifica o observable anterior.
+- Pipe- Função que serve para agrupar múltiplos operadores. Não modifica o observable anterior.
 
-Tap - Operador de serviços públicos. Usado para debugging. Não modifica o observable.
+- Tap - Operador de serviços públicos. Usado para debugging. Não modifica o observable.
 
-Map - Operador de transformação. Transforma o observable de acordo com a função passada. Retorna um observable modificado.
+- Map - Operador de transformação. Transforma o observable de acordo com a função passada. Retorna um observable modificado.
+
+- switchMap - Operador de Transformação. Cancela requisições de observables anteriores, emitindo valores apenas do Observable projetado mais recentemente.
+
+- filter - Operador de filtragem. Filtra os itens emitidos pelo Observable de origem, permitindo apenas aqueles que satisfaçam uma condição especificada.
+
+- debounceTime - Operador de filtragem. Retorna um Observable que atrasa as emissões do Observable de origem pelo tempo especificado.
+
+- distinctUntilChanged - Operador de filtragem. Retorna um Observable que emite todos os valores enviados pelo observable de origem se forem distintos em comparação com o último valor emitido pelo observable de resultado.
 
 #### Para saber mais: pipes do Angular
 
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,45 +1,51 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Item, Livro } from 'src/app/models/interfaces';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+} from 'rxjs';
+import { Item } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const PAUSA = 300;
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent implements OnDestroy {
-  protected campoBusca: string = '';
-  protected listaLivros: Livro[] = [];
-
-  private livro!: Livro;
-  private subscription: Subscription;
+export class ListaLivrosComponent {
+  protected campoBusca = new FormControl();
 
   constructor(private service: LivroService) {}
 
-  buscarLivros() {
-    this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: (items) => {
-        this.listaLivros = this.livrosResultadosParaLivros(items);
-      },
-      error: (error) => console.log('Error', error),
-      complete: () => console.log('complete'),
-    });
-  }
-
-  // .subscribe(data => this.config = {
-  //   heroesUrl: (data as any).heroesUrl,
-  //   textFile: (data as any).textFile,
-  // })
+  // é uma convenção utilizar o símbolo de dólar quando a variável é um observable
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map((items) => this.livrosResultadosParaLivros(items))
+  );
 
   livrosResultadosParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map((item) => {
       return new LivroVolumeInfo(item);
     });
   }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 }
+
+// switchmap
+// A ideia desse operador é trocar os valores e passar ao servidor só o último valor (B),
+// desconsiderando os valores anteriores (A).
+//
+// Exemplo
+// [Observable] ---- [switchMap] ---- observer
+//
+// Neste exemplo a cada dígito é feita uma chamada para o servidor,
+// mas com o switchMap essas chamadas são canceladas e é feita somente a última
+// isso reduz o número de requisições feitas
